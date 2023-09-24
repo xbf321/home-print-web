605ac305d7990d2b9c9f8fba95c247c3dfa60db9
xingbaifang <xingbaifang@meituan.com>
2023-09-24 12:40:16 +0800

    add print fail error

--- server/api/print.post.ts
+++ server/api/print.post.ts
@@ -2,5 +2,12 @@ import FilesService from '../service/files';
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { uid } = body;
-  return FilesService.print(uid);
+  let result = false;
+  try {
+    result = await FilesService.print(uid);
+  } catch (err) {
+    result = false;
+    sendMessageToPusher(`打印 ${uid} 失败`, err);
+  }
+  return result;
 });
