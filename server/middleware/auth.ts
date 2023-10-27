1084824dc3a99c0883547ca89102aae228ec031c
xingbaifang <xingbaifang@meituan.com>
2023-10-27 11:45:46 +0800

    ignore auth in localip

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,19 +1,24 @@
 // @ts-nocheck
+import ip from 'ip';
 export default defineEventHandler((event) => {
-  const { authUserName, authUserPassword } = useServerRuntimeConfig();
-  const headers = getRequestHeaders(event);
-  const authHeader = headers.authorization;
-  if (!authHeader) {
-    setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
-    setResponseStatus(event, 401);
-    event.node.res.end('No auth');
-  } else {
-    const requestAuth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    const [ requestUserName, requestUserPassword ] = requestAuth;
-    if (authUserName !== requestUserName && authUserPassword !== requestUserPassword) {
+  // 如果是内网环境，忽略登陆
+  const myIP = ip.address();
+  if (!!!myIP.match(/^192/)) {
+    const { authUserName, authUserPassword } = useServerRuntimeConfig();
+    const headers = getRequestHeaders(event);
+    const authHeader = headers.authorization;
+    if (!authHeader) {
       setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
       setResponseStatus(event, 401);
       event.node.res.end('No auth');
+    } else {
+      const requestAuth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const [ requestUserName, requestUserPassword ] = requestAuth;
+      if (authUserName !== requestUserName && authUserPassword !== requestUserPassword) {
+        setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
+        setResponseStatus(event, 401);
+        event.node.res.end('No auth');
+      }
     }
   }
 })
