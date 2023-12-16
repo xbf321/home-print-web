faa7f63b2c3bff7f10cef9ac14ad7d6d466fd25e
xingbaifang <xbf321@gmail.com>
2023-12-16 12:29:16 +0800

    Update auth.ts

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -4,17 +4,20 @@ export default defineEventHandler(async (event) => {
   if (!isDev) {
     const headers = getRequestHeaders(event);
     const authHeader = headers.authorization;
-    if (!authHeader) {
-      setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
-      setResponseStatus(event, 401);
-      event.node.res.end('No auth');
-    } else {
-      const requestAuth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const [ requestUserName, requestUserPassword ] = requestAuth;
-      if (authUserName !== requestUserName && authUserPassword !== requestUserPassword) {
+    const xRealIP = headers['x-real-ip'];
+    if (!/^1(2|9)(7|2)\./gi.test(xRealIP)) {
+      if (!authHeader) {
         setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
         setResponseStatus(event, 401);
         event.node.res.end('No auth');
+      } else {
+        const requestAuth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+        const [ requestUserName, requestUserPassword ] = requestAuth;
+        if (authUserName !== requestUserName && authUserPassword !== requestUserPassword) {
+          setHeader(event, 'WWW-Authenticate', 'Basic realm="Restricted"');
+          setResponseStatus(event, 401);
+          event.node.res.end('No auth');
+        }
       }
     }
   }
